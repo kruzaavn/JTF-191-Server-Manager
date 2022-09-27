12b44e2afc14516fc6ac5856a5315a018b332197
aleks <aleks.kruza@gmail.com>
2022-09-27 17:12:05 -0400

    working on installer code to gather default paths

--- installer/src/main.rs
+++ installer/src/main.rs
@@ -1,9 +1,11 @@
 use eframe::egui;
+use std::path::Path;
 use egui_extras::RetainedImage;
 
 fn main() {
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(300.0, 400.0)),
+        resizable: false,
         ..Default::default()
     };
     eframe::run_native(
@@ -13,15 +15,25 @@ fn main() {
     );
 }
 
+fn get_default_dcs_install_path() -> Option<String> {
+    return None
+}
+
+fn get_default_dcs_saved_games_path() -> Option<String> {
+    return None
+}
+
 struct MyApp {
-    path: String,
+    install_path: Option<String>,
+    saved_games_path: Option<String>,
     image: RetainedImage
 }
 
 impl Default for MyApp {
     fn default() -> Self {
         Self {
-            path: "".to_owned(),
+            install_path: get_default_dcs_install_path(),
+            saved_games_path: get_default_dcs_saved_games_path(),
             image: RetainedImage::from_image_bytes(
                 "JTF191d.png",
                 include_bytes!("JTF191d.png"),
@@ -31,17 +43,34 @@ impl Default for MyApp {
     }
 }
 
+
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
 
+
             self.image.show_scaled(ui, 0.1);
 
             ui.horizontal(|ui| {
-                ui.label("DCS Install Path: ");
-                ui.text_edit_singleline(&mut self.path);
+
+                if ui.button("DCS Install Path").clicked() {
+                    if let Some(path) = rfd::FileDialog::new().pick_folder() {
+                        self.install_path = Some(path.display().to_string());
+                    }
+                };
+            });
+            ui.horizontal(|ui| {
+                if ui.button("DCS Saved Games Path").clicked() {
+                    if let Some(path) = rfd::FileDialog::new().pick_folder() {
+                        self.saved_games_path = Some(path.display().to_string());
+                    }
+                };
             });
             if ui.button("Install Package").clicked() {
+
+                if self.install_path.is_some()  && self.saved_games_path.is_some() {
+                    println!("I've been clicked! ")
+                }
             }
         });
     }
